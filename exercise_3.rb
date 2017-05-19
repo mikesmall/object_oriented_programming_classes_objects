9134bac1304b203960995330e469671f66f48c07
mikesmall <michael.l.a.small@gmail.com>
2017-05-19 16:39:44 -0400

    Exercise 3 steps 1-5 done

---
+++ exercise_3.rb
@@ -0,0 +1,11 @@
+# Appending "_arg" to argument names, to differentiate.
+
+class Player
+
+  def initialize(lives_arg, gold_coins_arg, health_points_arg)
+    @lives = 5
+    @gold_coins = 0
+    @health_points = 10
+  end
+
+end # end of Player class 
