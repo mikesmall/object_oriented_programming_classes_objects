ea62fced7360076f68bccb228c6596cd9b710e01
mikesmall <michael.l.a.small@gmail.com>
2017-05-19 15:49:01 -0400

    Exercise 2, steps 1 and 2 complete

---
+++ exercise_2/cat.rb
@@ -0,0 +1,10 @@
+#  Adding "_arg" to arguments, just to keep track.
+#  I still get confused by different things having the same name.
+
+class Cat
+  def initialize(name_arg, preferred_food_arg, meal_time_arg)
+    @name = name_arg
+    @preferred_food = preferred_food_arg
+    @meal_time = meal_time_arg
+  end
+end
