05b4ea3ddbdb37a1c5db3e2c66424d16db9bff23
mikesmall <michael.l.a.small@gmail.com>
2017-05-19 18:19:20 -0400

    Boilerplate class stuff for Exercise 4: Paperboy

---
+++ exercise_4_paperboy/paperboy.rb
@@ -0,0 +1,14 @@
+class Paperboy
+
+  def initialize(name, experience, earnings)
+    @name = name
+    @experience = experience
+    @earnings = earnings
+    attr_reader :earnings
+  end#initialize
+
+
+
+
+
+end #class:Paperboy
