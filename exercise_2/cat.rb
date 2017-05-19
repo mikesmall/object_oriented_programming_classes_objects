14007a0a9202282cd9a1b1362eb1401a8443eb63
mikesmall <michael.l.a.small@gmail.com>
2017-05-19 16:15:44 -0400

    Corrections made to previous steps

--- exercise_2/cat.rb
+++ exercise_2/cat.rb
@@ -12,20 +12,33 @@ class Cat
   # the hour of the day with AM or PM that this cat eats.
   # The return value should be something like "11 AM" or "3 PM"
   def eats_at
+
     if @meal_time <= 11
-      puts "#{ @name} eats at #{ @meal_time }:00 AM."
-    elsif @meal_time = 12
+      puts "#{ @name } eats at #{ @meal_time }:00 AM."
+
+    elsif @meal_time == 12
       puts "#{ @name } eats at #{ @meal_time }:00 PM."
+
     elsif @meal_time >= 13
-      puts "#{ name } eats at #{ @meal_time - 12 }:00 PM."
+      puts "#{ @name } eats at #{ @meal_time - 12 }:00 PM."
+
+    end
   end
 
-
-
+  # Add another instance method called meow that returns
+  # a string of the cat telling you all about itself.
+  # The return value should be something like "My name
+  # is Sparkles and I eat tuna at 11 AM"
+  def meow
+      puts "My name is #{ @name }. I eat #{ @preferred_food } at #{ eats_at }"
+  end
 end
 
 # Create two instances of the Cat class in your file
 # They should each have unique names, preferred foods and meal times
 # Let's assume meal_time is an integer from 0 to 23
-Cat.new('Mavis', 'potato chips', 9)
-Cat.new('Batman', 'canned tuna', 17)
+mavis = Cat.new('Mavis', 'potato chips', 9)
+batman = Cat.new('Batman', 'canned tuna', 17)
+
+mavis.eats_at
+mavis.meow
